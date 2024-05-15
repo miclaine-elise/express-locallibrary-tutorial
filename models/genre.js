14a98acb160824d6e88a853850e1181b2d67c142
Miclaine Emtman <clainie@gmail.com>
2024-05-15 10:12:49 -0700

    First version of application moved into GitHub

---
+++ models/genre.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 100,
+        ref: "Book", required: true
+    }
+});
+
+// Virtual for bookinstance's URL
+GenreSchema.virtual("url").get(function () {
+    // We don't use an arrow function as we'll need the this object
+    return `/catalog/Genre/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", GenreSchema);
